3d39973c08fc71788fae38ae3be42e650d147e7c
Akhila Muthyala <amuthyal@asu.edu>
2025-04-30 16:51:32 -0500

    Fix TypeScript and build config for successful Vercel deployment

--- src/types/jest-axe.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'jest-axe' {
-    import { AxeResults } from 'axe-core';
-    import { MatcherFunction } from 'expect';
-  
-    export const axe: (container: HTMLElement) => Promise<AxeResults>;
-    export const toHaveNoViolations: MatcherFunction<[AxeResults]>;
-  }
-  
